a9b539345c8704d13441fb0bab299895a36b9d23
bamboo110 <9bbambangsetyo.b@gmail.com>
2019-12-03 19:40:34 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const burger = document.querySelector('.burger');
+const navlink = document.querySelector('.navLink');
+const bar = document.querySelector('.bar');
+const close = document.querySelector('.close');
+const nav = document.querySelector('nav');
+
+burger.addEventListener('click', () =>{
+
+  if (navlink.style.maxHeight) {
+    navlink.style.maxHeight = null;
+    nav.style.backgroundColor = '#F2F2F2';
+  } else {
+    navlink.style.maxHeight = navlink.scrollHeight + 'px';
+    nav.style.backgroundColor = 'rgba(192, 121, 150, 0.774)';
+  };
+
+  
+
+
+});
+
